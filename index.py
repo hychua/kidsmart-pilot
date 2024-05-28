784f1527447aab54bd96d426e3330377318a1a9a
hychua <85439265+hychua@users.noreply.github.com>
2024-05-28 21:19:16 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -88,7 +88,66 @@ METRIC_DICT = {
 
 
 # Create Pages
-layout1 = html.Div(
+
+# end create pages
+        
+
+
+
+
+layout_index = html.Div([
+    
+                html.Div([
+                    
+                html.Img(id="logo", src=app.get_asset_url("logo.png")),
+                
+                    html.Div([
+                        html.H1('Kidsmart Sales and Inventory Monitoring Dashboard'),
+                        html.P(
+                            id="description",
+                            children="† Please select dashboard from the tabs below. \
+                            ",
+                        ),
+                                                
+                    ],style={'display':'inline-block','margin-left':50}),
+                            
+                        ],
+                        style={}),
+
+    html.Div([
+        dcc.Tabs(id='tabs', value='sales-dash', children=[
+        dcc.Tab(label='Sales Dashboard', value='sales-dash',style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Inventory Dashboard', value='inventory-dash',style=tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Upload Data', value='upload-data',style=tab_style, selected_style=tab_selected_style),
+
+        ]),
+    
+    ], style={}),
+    html.Div(id='tabs-content', style={}),
+
+])
+
+url_bar_and_content_div = html.Div([
+    
+    dcc.Location(id='url', refresh=False),
+    layout_index,
+    html.Div(id='page-content')
+])
+
+
+app.layout = url_bar_and_content_div
+
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
+
+
+
+
+# Tab callbacks
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'sales-dash':
+        return html.Div(
     id="root",
     children=[
         html.Div(
@@ -232,9 +291,8 @@ layout1 = html.Div(
         
     ],
 )
-
-
-layout2 = html.Div(
+    elif tab == 'inventory-dash':
+        return html.Div(
     id="root",
     children=[
         html.Div(
@@ -528,8 +586,8 @@ layout2 = html.Div(
 
     ],
 )
-
-layout3 = html.Div([    html.Div(' ',
+    elif tab == 'upload-data':   
+        return html.Div([    html.Div(' ',
         style={'backgroundColor':'rgb(0,123,255)','height':42}),
         html.Div([
             html.H2('Upload Data',
@@ -574,9 +632,6 @@ layout3 = html.Div([    html.Div(' ',
                     style={'float':'left','display': 'inline-block',
                            'margin-left':50})
             ])
-# end create pages
-        
-
 
 # Create call back functions
 
@@ -1734,65 +1789,6 @@ def set_worse_turnover_graph(year, selected_brand,selected_region):
     )
     return fig         
 # end from dashboard.py
-
-layout_index = html.Div([
-    
-                html.Div([
-                    
-                html.Img(id="logo", src=app.get_asset_url("logo.png")),
-                
-                    html.Div([
-                        html.H1('Kidsmart Sales and Inventory Monitoring Dashboard'),
-                        html.P(
-                            id="description",
-                            children="† Please select dashboard from the tabs below. \
-                            ",
-                        ),
-                                                
-                    ],style={'display':'inline-block','margin-left':50}),
-                            
-                        ],
-                        style={}),
-
-    html.Div([
-        dcc.Tabs(id='tabs', value='sales-dash', children=[
-        dcc.Tab(label='Sales Dashboard', value='sales-dash',style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Inventory Dashboard', value='inventory-dash',style=tab_style, selected_style=tab_selected_style),
-        dcc.Tab(label='Upload Data', value='upload-data',style=tab_style, selected_style=tab_selected_style),
-
-        ]),
-    
-    ], style={}),
-    html.Div(id='tabs-content', style={}),
-
-])
-
-url_bar_and_content_div = html.Div([
-    
-    dcc.Location(id='url', refresh=False),
-    layout_index,
-    html.Div(id='page-content')
-])
-
-
-app.layout = url_bar_and_content_div
-
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
-
-
-
-
-# Tab callbacks
-@app.callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
-def render_content(tab):
-    if tab == 'sales-dash':
-        return layout1
-    elif tab == 'inventory-dash':
-        return layout2
-    elif tab == 'upload-data':   
-        return layout3
-
 
 if __name__ == '__main__':
     app.run_server(debug=False)
