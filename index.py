0fd4390655f825627f462bab8998b8d0dc11ae34
hychua <85439265+hychua@users.noreply.github.com>
2024-05-29 15:49:17 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 # from dashboard.py
 from dash import dash_table
@@ -21,7 +21,9 @@ from analytics import( create_plot_metric,
 from app import app
 import os
 
-
+import base64
+import datetime
+import io
 
 # define tab styles
 tab_style = {
@@ -90,11 +92,7 @@ METRIC_DICT = {
 # Create Pages
 
 # end create pages
-        
-
-
-
-
+      
 layout_index = html.Div([
     
                 html.Div([
@@ -130,81 +128,7 @@ layout_index = html.Div([
 
 app.layout = layout_index
 
-layout3 = html.Div([    html.Div(' ',
-                style={'backgroundColor':'rgb(0,123,255)','height':42}),
-                html.Div([
-                    dcc.Upload(html.Button('Upload orders.csv File')),
-
-                    html.Hr(),
-                
-                    dcc.Upload(html.A('Upload orders.csv File')),
-                
-                    html.Hr(),
-                
-                    dcc.Upload([
-                        'Drag and Drop or ',
-                        html.A('Select a File')
-                    ], style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center'
-                    }),
-                dcc.Upload(html.Button('Upload inventory.csv File')),
-                
-                    html.Hr(),
-                
-                    dcc.Upload(html.A('Upload inventory.csv File')),
-                
-                    html.Hr(),
-                
-                    dcc.Upload([
-                        'Drag and Drop or ',
-                        html.A('Select a File')
-                    ], style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center'
-                    }),
-                dcc.Upload(html.Button('Upload category.csv File')),
-                
-                    html.Hr(),
-                
-                    dcc.Upload(html.A('Upload category.csv File')),
-                
-                    html.Hr(),
-                
-                    dcc.Upload([
-                        'Drag and Drop or ',
-                        html.A('Select a File')
-                    ], style={
-                        'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
-                        'borderWidth': '1px',
-                        'borderStyle': 'dashed',
-                        'borderRadius': '5px',
-                        'textAlign': 'center'
-                    }
-                                    
-                                    )],
-                                            style={'float':'left','display': 'inline-block',
-                                                   'margin-left':50})
-                                    ])
-
-# Tab callbacks
-@app.callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
-def render_content(tab):
-    if tab == 'sales-dash':
-        return html.Div(
+layout1 = html.Div(
                 id="sales-dash",
                 children=[
                     html.Div(
@@ -348,8 +272,8 @@ def render_content(tab):
                     
                 ],
             )
-    elif tab == 'inventory-dash':
-        return html.Div(
+
+layout2 = html.Div(
                     id="inv-dash",
                     children=[
                         html.Div(
@@ -641,7 +565,103 @@ def render_content(tab):
 
 
                     ],
-                )
+                )    
+    
+layout3 = html.Div([    html.Div(' ',
+                style={'backgroundColor':'rgb(0,123,255)','height':42}),
+                html.Div([
+                                    
+                    dcc.Upload(html.A('Upload orders.csv File')),
+                
+                    html.Hr(),
+                
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files')
+                        ]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=True
+                    ),
+                    html.Div(id='output-data-upload'),
+                
+                    html.Hr(),
+                
+                    dcc.Upload(html.A('Upload inventory.csv File')),
+                
+                    html.Hr(),
+                
+                    dcc.Upload(
+                        id='upload-data2',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files')
+                        ]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=True
+                    ),
+                    html.Div(id='output-data-upload2'),
+                                
+                    html.Hr(),
+                
+                    dcc.Upload(html.A('Upload category.csv File')),
+                
+                    html.Hr(),
+                
+                    dcc.Upload(
+                        id='upload-data3',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files')
+                        ]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                            'margin': '10px'
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=True
+                    ),
+                    html.Div(id='output-data-upload3'),
+                        ],
+                                            style={'float':'left','display': 'inline-block',
+                                                   'margin-left':50})
+                                    ])
+
+# Tab callbacks
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'sales-dash':
+        return layout1
+    elif tab == 'inventory-dash':
+        return layout2
     elif tab == 'upload-data':   
         return layout3
 
@@ -1802,6 +1822,77 @@ def set_worse_turnover_graph(year, selected_brand,selected_region):
     return fig         
 # end from dashboard.py
 
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+# start upload callbacks
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+    
+@app.callback(Output('output-data-upload2', 'children'),
+              Input('upload-data2', 'contents'),
+              State('upload-data2', 'filename'),
+              State('upload-data2', 'last_modified'))
+def update_output2(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+    
+@app.callback(Output('output-data-upload3', 'children'),
+              Input('upload-data3', 'contents'),
+              State('upload-data3', 'filename'),
+              State('upload-data3', 'last_modified'))
+def update_output3(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+# end upload callbacks
 
 app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
